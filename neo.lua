b4de9ee5e4a7033663183e775f3b3695d6c49a16
zhangtao <ztgoforit@163.com>
2018-11-03 01:05:29 -0400

    tx, block dissector, TODO: atrribute and scripts show tree

--- neo.lua
+++ neo.lua
@@ -24,6 +24,19 @@ do
     TX_CONTRACT     = 0x80
     TX_INVOCATION   = 0xd1
 
+    ATTR_CONTRACTHASH   = 0x00
+    ATTR_ECDH02         = 0x02
+    ATTR_ECDH03         = 0x03
+    ATTR_SCRIPT         = 0x20
+    ATTR_VOTE           = 0x30
+    ATTR_CERTURL        = 0x80
+    ATTR_DESCRIPURL     = 0x81
+    ATTR_DESCRIP        = 0x90
+    ATTR_HASH1          = 0xa1
+    ATTR_HASH15         = 0xaf
+    ATTR_REMARK0        = 0xf0
+    ATTR_REMARK15       = 0xff
+
     DATA_TX         = 0x01
     DATA_BLOCK      = 0x02
     DATA_CONCENSUS  = 0xe0
@@ -60,11 +73,36 @@ do
         [TX_CONTRACT]   = "ContractTx",
         [TX_INVOCATION] = "InvocationTx",
     }
+    ATTR_USAGE_TYPE = {
+        [ATTR_CONTRACTHASH]   = "ContractHash",
+        [ATTR_ECDH02]         = "ECHH02",
+        [ATTR_ECDH03]         = "ECHH03",
+        [ATTR_SCRIPT]         = "Script",
+        [ATTR_VOTE]           = "Vote",
+        [ATTR_CERTURL]        = "CerUrl",
+        [ATTR_DESCRIPURL]     = "DescriptionUrl",
+        [ATTR_DESCRIP]        = "Description",
+        [ATTR_HASH1]          = "Hash1",
+        [ATTR_HASH15]         = "Hash15",
+        [ATTR_REMARK0]        = "Remark0",
+        [ATTR_REMARK15]       = "Remark15",
+    }
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     local function dprint(...)
         info(table.concat({"Lua: ", ...}, " "))
+    end
+    ---------------------------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------------------------------
+    local function get_attr_type_str(type)
+        if ATTR_HASH1 <= type and type <= ATTR_HASH15 then
+            return "Hash"..tostring(type)
+        elseif ATTR_REMARK0 <= type and type <= ATTR_REMARK15 then
+            return "Remark"..tostring(type)
+        end
+        return ATTR_USAGE_TYPE[type]
     end
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
@@ -98,15 +136,36 @@ do
         local script_tree = tree:add(neop2p_script, buffer(offset, scriptlen + 1), name.."Script")
 
         script_tree:add(neop2p_script.fields.scriptlen, buffer(offset, 1), scriptlen)
+        if scriptlen == 0 then
+            return 1
+        end
         script_tree:add(neop2p_script.fields.script, buffer(offset + 1, scriptlen), tostring(buffer(offset + 1, scriptlen)))
         return scriptlen + 1
+    end
+
+    local neop2p_scripts = Proto("scripts", "scripts")
+
+    neop2p_scripts.fields.count = ProtoField.uint8("scripts.count", "COUNT", base.DEC)
+
+    local function neop2p_scripts_dissector(buffer, pinfo, tree, offset)
+        local index = 0
+        local local_offset = 0
+        local count = buffer(offset, 1):le_uint()
+        tree:add(neop2p_scripts.fields.count, buffer(offset, 1), count)
+        local_offset = local_offset + 1
+        while (index < count) do
+            local result = neop2p_script_dissector(buffer, pinfo, tree, offset + 1 + index * count, "script")
+            local_offset = local_offset + result
+            index = index + 1
+        end
+        return local_offset
     end
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     local neop2p_coin = Proto("Coin", "coin reference")
 
-    neop2p_coin.fields.prehash = ProtoField.string("coin.prehash", "PREHASH", base.DEC)
+    neop2p_coin.fields.prehash = ProtoField.string("coin.prehash", "PREHASH", base.ASCII)
     neop2p_coin.fields.preindex = ProtoField.uint16("coin.preindex", "PREINDEX", base.DEC)
 
     local function neop2p_coin_dissector(buffer, pinfo, tree, offset)
@@ -119,17 +178,54 @@ do
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     local neop2p_coins = Proto("Coins", "coin reference array")
+    
+    neop2p_coins.fields.count = ProtoField.uint8("coins.count", "COUNT", base.DEC)
 
     local function neop2p_coins_dissector(buffer, pinfo, tree, offset)
         local index = 0
         local count = buffer(offset, 1):le_uint()
-        local coins_tree = tree:add(neop2p_coins, buffer(offset, count * 34), "COINS")
-
+        local coins_tree = tree:add(neop2p_coins, buffer(offset, 1 + count * 34), "COINS")
+        coins_tree:add(neop2p_coins.fields.count, buffer(offset, 1), count)
         while (index < count) do
-            local result = neop2p_coin_dissector(buffer, pinfo, coins_tree, offset + index * count)
+            local result = neop2p_coin_dissector(buffer, pinfo, coins_tree, offset + 1 + index * count)
             index = index + 1
         end
         return count * 34 + 1
+    end
+    ---------------------------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------------------------------
+    local neop2p_output = Proto("Output", "transaction output")
+
+    neop2p_output.fields.assetid = ProtoField.string("output.assetid", "ASSETID", base.ASCII)
+    neop2p_output.fields.value = ProtoField.uint64("output.value", "VALUE", base.DEC)
+    neop2p_output.fields.scripthash = ProtoField.uint16("output.scripthash", "SCRIPTHASH", base.DEC)
+
+    local function neop2p_output_dissector(buffer, pinfo, tree, offset)
+        local output_tree = tree:add(neop2p_coin, buffer(offset, 60), "Output")
+        output_tree:add(neop2p_output.fields.assetid, buffer(offset, 32), tostring(buffer(offset, 32)))
+        output_tree:add(neop2p_output.fields.value, buffer(offset + 32, 8), buffer(offset + 32 + 8, 8):le_uint64())
+        output_tree:add(neop2p_output.fields.scripthash, buffer(offset + 40, 20), tostring(buffer(offset, 20)))
+        return 60
+    end
+
+    local neop2p_outputs = Proto("Outpus", "transaction outpus")
+
+    neop2p_outputs.fields.count = ProtoField.uint8("outputs.count", "COUNT", base.DEC)
+
+    local function neop2p_outputs_dissector(buffer, pinfo, tree, offset)
+        local index = 0
+        local local_offset = 0
+        local count = buffer(offset, 1):le_uint()
+        local outputs_tree = tree:add(neop2p_outputs, buffer(offset + local_offset, 1 + count * 60), "Outputs")
+        outputs_tree:add(neop2p_outputs.fields.count, buffer(offset + local_offset, 1), count)
+        local_offset = local_offset + 1
+        while (index < count) do
+            local result = neop2p_coin_dissector(buffer, pinfo, coins_tree, offset + local_offset)
+            local_offset = local_offset + result
+            index = index + 1
+        end
+        return local_offset
     end
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
@@ -141,7 +237,40 @@ do
     neop2p_attr.fields.data = ProtoField.string("attr.data", "DATA", base.ASCII)
 
     local function neop2p_attr_dissector(buffer, pinfo, tree, offset)
-    
+        local usage = buffer(offset, 1):le_uint()
+        if usage == ATTR_CONTRACTHASH or usage == ATTR_VOTE or usage == ATTR_ECDH02 
+        or usage == ATTR_ECDH03 or (ATTR_HASH1 <= usage and usage <= ATTR_HASH15) then
+            local length = 32
+            local attr_tree = tree:add(neop2p_attr, buffer(offset, 1 + length), "Attribute")
+            attr_tree:add(neop2p_attr.fields.usage, buffer(offset, 1), get_attr_type_str(usage))
+            attr_tree:add(neop2p_attr.fields.data, buffer(offset + 1, length), tostring(buffer(offset + 1, length)))
+            return length + 1
+        end
+        local length = buffer(offset + 1, 1):le_uint()
+        local attr_tree = tree:add(neop2p_attr, buffer(offset, 2 + length), "Attribute")
+        attr_tree:add(neop2p_attr.fields.usage, buffer(offset, 1), usage)
+        attr_tree:add(neop2p_attr.fields.usage, buffer(offset + 1, 1), length)
+        attr_tree:add(neop2p_attr.fields.data, buffer(offset + 2, length), tostring(buffer(offset + 1, length)))
+        return length + 2
+    end
+
+    local neop2p_attrs = Proto("Attributes", "transaction attributes")
+
+    neop2p_attrs.fields.count = ProtoField.uint8("attrs.count", "COUNT", base.DEC)
+
+    local function neop2p_attrs_dissector(buffer, pinfo, tree, offset)
+        local index = 0
+        local local_offset = 0
+        local count = buffer(offset, 1):le_uint()
+        -- local attrs_tree = tree:add(neop2p_attrs, buffer(offset + local_offset, count * 60), "Outputs")
+        tree:add(neop2p_outputs.fields.count, buffer(offset + local_offset, 1), count)
+        local_offset = local_offset + 1
+        while (index < count) do
+            local result = neop2p_attr_dissector(buffer, pinfo, tree, offset + local_offset)
+            local_offset = local_offset + result
+            index = index + 1
+        end
+        return local_offset
     end
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
@@ -150,22 +279,54 @@ do
 
     neop2p_tx.fields.type = ProtoField.uint8("tx.type", "TYPE", base.DEC, TX_TYPE)
     neop2p_tx.fields.version = ProtoField.uint8("tx.version", "VERSION", base.DEC)
-    ----exclusive
+    ------exclusive
     --miner
     neop2p_tx.fields.nonce = ProtoField.uint32("tx.nonce", "NONCE", base.DEC)
     --claims
-    --neop2p_tx.fields.claims = coins
+    --neop2p_tx.fields.claims = neop2p_coins
     --invocation
-    --neop2p_tx.fields.script = script
-    neop2p_tx.fields.gas = ProtoField.uint8("tx.gas", "GAS", base.DEC)
-    --attributes
-    neop2p_tx.fields.attributes =
-
-    local inputs = Proto("inputs", "tx inputs")
-    inputs.fields.input = ProtoField.string("tx.inputs", "INPUTS", base.ASCII)
-    local outputs = Proto("outputs", "tx outpus")
-    outputs.fields.outputs = ProtoField.string("tx.outputs", "OUTPUTS", base.ASCII)
+    --neop2p_tx.fields.script = neop2p_script
+    neop2p_tx.fields.gas = ProtoField.uint64("tx.gas", "GAS", base.DEC)
+    ------attributes
+    --neop2p_tx.fields.attributes = neop2p_attr
+    ------inputs
+    --neop2p_tx.fields.inputs = neop2p_coins
+    ------outpus
+    --neop2p_tx.fields.outputs = neop2p_outputs
     neop2p_tx.fields.script = ProtoField.string("tx.script", "SCRIPT", base.ASCII)
+
+    local function neop2p_tx_dissector(buffer, pinfo, tree)
+        local offset = 0
+        local tx_tree = tree:add(neop2p_tx, buffer, "Tx")
+        local type = buffer(offset, 1):le_uint()
+        tx_tree:add(neop2p_tx.fields.type, buffer(offset, 1), type)
+        offset = offset + 1
+        tx_tree:add(neop2p_tx.fields.version, buffer(offset, 1), buffer(offset, 1):le_uint())
+        offset = offset + 1
+        ----exclusive
+        if type == TX_MINER then
+            tx_tree:add(neop2p_tx.fields.nonce, buffer(offset, 4), buffer(offset, 4):le_uint())
+            offset = offset + 4
+        elseif type == TX_CLAIM then
+            local result = neop2p_coins_dissector(buffer, pinfo, tx_tree, offset)
+            offset = offset + result
+        elseif type == TX_INVOCATION then
+            local result = neop2p_script_dissector(buffer, pinfo, tx_tree, offset, "Script")
+            offset = offset + result
+            tx_tree:add(neop2p_tx.fields.gas, buffer(offset, 8), buffer(offset, 8):le_uint())
+            offset = offset + 8
+        end
+        ----attributes
+        local result = neop2p_attrs_dissector(buffer, pinfo, tx_tree, offset)
+        offset = offset + result
+        result = neop2p_coins_dissector(buffer, pinfo, tx_tree, offset)
+        offset = offset + result
+        result = neop2p_outputs_dissector(buffer, pinfo, tx_tree, offset)
+        offset = offset + result
+        result = neop2p_scripts_dissector(buffer, pinfo, tx_tree, offset)
+        offset = offset + result
+        return offset
+    end 
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
@@ -182,6 +343,7 @@ do
     neop2p_block.fields.scriptcount = ProtoField.uint8("block.scriptcount", "scriptcount", base.DEC)
     neop2p_block.fields.script = ProtoField.string("block.script", "SCRIPT", base.ASCII)
     neop2p_block.fields.txs = ProtoField.string("block.txs", "TXS", base.ASCII)
+    neop2p_block.fields.txscount = ProtoField.uint8("block.txscount", "TXSCOUNT", base.DEC)
 
     local function neop2p_block_dissector(buffer, pinfo, tree)
         local len = buffer:len()
@@ -210,6 +372,15 @@ do
         result = neop2p_script_dissector(buffer, pinfo, block_tree, offset, "verify")
         offset = offset + result
         block_tree:add(neop2p_block.fields.txs, buffer(offset, len - offset), tostring(buffer(offset, len - offset)))
+        local txs_count = buffer(offset, 1):le_uint()
+        block_tree:add(neop2p_block.fields.txscount, buffer(offset, 1), txs_count)
+        offset = offset + 1
+        local index = 0
+        while (index < txs_count) do
+            local result = neop2p_tx_dissector(buffer(offset, len - offset), pinfo, block_tree)
+            offset = offset + result
+            index = index + 1
+        end
     end
     ---------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------
